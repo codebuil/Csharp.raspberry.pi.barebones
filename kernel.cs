a2b9047cfae370365b1e1b90bfb2cb96bdfbcecc
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-23 20:46:27 +0100

    Add files via upload

--- kernel.cs
+++ kernel.cs
@@ -1,23 +1,34 @@
 namespace os
 {
 	public class oss
-	{
-		  unsafe public static void kernel_main() 
+	{	public static int rgb(int r, int g,int b){
+				return (r & 63)<<11 | (g & 63) << 5 | (b & 63);
+		}
+		
+		unsafe public static void clear(int colorss) 
 		  {
-			   // Clear the screen 
-			   
+			  			   // Clear the screen 
+			   int ii=colorss & 0xff;
+			   int iii=((colorss >> 8) & 0xff);
+			   char c1=(char) ii;
+			   char c2=(char) iii;
 			   char *fbp=(char * )0x04100000;
-			   char i15=(char)15;
-			   char i0=(char)0;
-			   for(int i = 0; i < 640 * 480 *2; i=i+2){
-			    *((char *)(fbp + i)) = i15;
-			    *((char *)(fbp + i+1)) = i0;
+			   for(int i = 0; i < 640 * 480 * 2 ; i=i+2){
+			    *((char *)(fbp + i)) =c1;
+			    *((char *)(fbp + i+1)) =c2;
+			    
 			   
 			}
+		  }
+		  public static void kernel_main() 
+		  {
+			   // Clear the screen 
+			int color1=rgb(0,0,63);
+			clear(color1);
 			while(true){}
 			} 
 
-		  unsafe public static void Main() 
+		  public static void Main() 
 		  {
 			 kernel_main();
 			}  
