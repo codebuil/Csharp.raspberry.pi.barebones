7fc1bc9d9708a52000d61dbe55f9296dcecc7a9b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-23 09:40:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace screens
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+
+        {
+           string memory = "hello world...\n";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+			Console.Write(memory);
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- builder.sh
+++ builder.sh
@@ -1,12 +1,2 @@
-rm *.o
-rm *.elf
-rm *.img
-rm *.exe
-rm *.dll
-as -c -o boot.o boot.S
-mcs -t:library -unsafe kernel.cs -o kernel.dll
-mkbundle --static -o kernel.o kernel.dll
-#mono --aot=static kernel
-ld -T link.ld boot.o kernel.o -o kernel.elf -nostdlib
-objcopy kernel.elf -O binary kernel.img
-qemu-system-arm -m 128 -M raspi2 -serial stdio -kernel kernel.elf
+mcs Program.cs -o program.exe
+mkbundle -o program program.exe
